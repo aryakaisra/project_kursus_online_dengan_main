5f10b2ae858dc3b5ba53ad10790140a998b68a34
aryakaisra <aryakaisra123@gmail.com>
2025-07-09 14:27:16 +0800

    Delete project_kursus_online directory

--- project_kursus_online/controller/main_controller.py
+++
@@ -1,8 +0,0 @@
-
-from service.auth_service import login_user
-
-def login_controller(username, role):
-    if login_user(username, role):
-        print(f"{username} berhasil login sebagai {role}")
-    else:
-        print("Login gagal.")
